c57830203bf965a43455ff62bf9eb3de8ec71912
oshotokill <nlfctx@outlook.com>
2016-04-08 10:29:54 +0800

    initial state

---
+++ store/reducers/items.js
@@ -0,0 +1,45 @@
+const initState = [
+  {
+    header: {
+      title: "第一个卡片",
+      subtitle: "This is the first card",
+      avatar: ""
+    },
+
+    media: {
+      overlay: {
+        title: "第一个卡片",
+        subtitle: "This is the first card",
+        
+      }
+    },
+
+    text: "inspired by the communtiy",
+
+    action: <RaisedButton label="hello" primary={true} />
+  },
+
+  {
+    header: {
+      title: "第二个卡片",
+      subtitle: "This is the second card",
+      avatar: ""
+    },
+
+    media: {
+      overlay: {
+        title: "第二个卡片",
+        subtitle: "This is the second card",
+        
+      }
+    },
+
+    text: {
+
+    },
+
+    action: {
+
+    }
+  }
+]
