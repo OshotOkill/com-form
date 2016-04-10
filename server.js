2d3547722ffa0dce2ce785d7fcc19e7e63292770
Norn Chronos <nlfctx@outlook.com>
2016-04-10 22:51:27 +0800

    ES6 for server.js

--- server.js
+++ server.js
@@ -1,22 +1,21 @@
-var webpack = require('webpack')
-var webpackDevMiddleware = require('webpack-dev-middleware')
-var webpackHotMiddleware = require('webpack-hot-middleware')
-var config = require('./webpack.config')
+const webpack = require('webpack')
+const webpackDevMiddleware = require('webpack-dev-middleware')
+const webpackHotMiddleware = require('webpack-hot-middleware')
+const config = require('./webpack.config')
 
-var express = require('express')
-var app = express()
-var port = 3000
-var compiler = webpack(config)
+const express = require('express')
+const app = express()
+const port = 3000
+const compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
 
-app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html')
+app.get('/', (req, res) => {
+  res.sendFile(`${__dirname}/index.html`)
 })
 
-app.listen(port, function(err) {
-  if (err) console.error(err)
-  else console.log('listening port 3000')
+app.listen(port, (err) => {
+  err ? console.log(err) : console.log('listening port 3000')
 })
 
