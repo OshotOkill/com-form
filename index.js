b681ddbf6166801cba626959f199fed095ada486
oshotokill <nlfctx@outlook.com>
2016-04-11 10:09:48 +0800

    inject data to configStore

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import configStore from './store'
 import App from './container/app'
+import configStore from './store'
+import data from './data'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 
 
 injectTapEventPlugin()
 
 render(
-  <Provider store={ configStore(store) }>
+  <Provider store={ configStore(data) }>
     <App />
   </Provider>, 
   document.getElementById('root')
