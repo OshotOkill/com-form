82969839abb8c8882db0d0e95e3ad128bf9205ef
oshotokill <nlfctx@outlook.com>
2016-05-06 11:50:28 +0800

    Digging into font parsing problems

--- webpack-isomorphic-config.js
+++ webpack-isomorphic-config.js
@@ -1,57 +1,59 @@
 var webpack_isomorphic_tools_plugin = require('webpack-isomorphic-tools/plugin');
 
 module.exports = {
+	// debug: true,
+	
   assets: {
     images: {
       extensions: ['png', 'jpg', 'jpeg', 'gif', 'ico'],
-			// parser: webpack_isomorphic_tools_plugin.url_loader_parser
+			parser: webpack_isomorphic_tools_plugin.url_loader_parser
     },
 		
 		svg: {
 			extensions: ['svg'],
-		  // parser: webpack_isomorphic_tools_plugin.url_loader_parser
+		  parser: webpack_isomorphic_tools_plugin.url_loader_parser
 		},
 		
-		styles: {
-      extensions: ['css', 'sass'],
-      filter: function(module, regular_expression, options, log) {
+		// styles: {
+    //   extensions: ['css'],
+    //   filter: function(module, regular_expression, options, log) {
+		// 		if (options.development) {
+		// 			return webpack_isomorphic_tools_plugin.style_loader_filter(module, regular_expression, options, log);
+		// 		}
+		// 	},
+
+		// 	path: webpack_isomorphic_tools_plugin.style_loader_path_extractor,
+
+		// 	parser: webpack_isomorphic_tools_plugin.css_loader_parser
+    // },
+    
+    style_modules: {
+      extensions: ['css'],
+      filter: function(module, regex, options, log) {
 				if (options.development) {
-					return webpack_isomorphic_tools_plugin.style_loader_filter(module, regular_expression, options, log);
-				}
+					return webpack_isomorphic_tools_plugin.style_loader_filter(module, regex, options, log);
+				} 
+				return regex.test(module.name);				
 			},
 
-			path: webpack_isomorphic_tools_plugin.style_loader_path_extractor,
-
-			parser: webpack_isomorphic_tools_plugin.css_loader_parser
-    },
-    
-    // style_modules: {
-    //   extensions: ['css', 'sass'],
-    //   filter: function(module, regex, options, log) {
-		// 		if (options.development) {
-		// 			return webpack_isomorphic_tools_plugin.style_loader_filter(module, regex, options, log);
-		// 		} 
-		// 		return regex.test(module.name);				
-		// 	},
-
-		// 	path: function(module, options, log) {
-		// 		if (options.development) {
-		// 			return webpack_isomorphic_tools_plugin.style_loader_path_extractor(module, options, log);
-		// 		}
-		// 		return module.name;
-		// 	},
+			path: function(module, options, log) {
+				if (options.development) {
+					return webpack_isomorphic_tools_plugin.style_loader_path_extractor(module, options, log);
+				}
+				return module.name;
+			},
 			
-		// 	parser: function(module, options, log) {
-		// 		if (options.development) {
-		// 			return webpack_isomorphic_tools_plugin.css_modules_loader_parser(module, options, log);
-		// 		}
-		// 		return module.source;
-		// 	}
-    // },
+			parser: function(module, options, log) {
+				if (options.development) {
+					return webpack_isomorphic_tools_plugin.css_modules_loader_parser(module, options, log);
+				}
+				return module.source;
+			}
+    },
 		
     fonts: {
 			extensions: ['eot', 'ttf', 'woff', 'woff2'],
-			// parser: webpack_isomorphic_tools_plugin.url_loader_parser
+			parser: webpack_isomorphic_tools_plugin.url_loader_parser
     }
   }    
 }
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,7 +1,6 @@
 var path = require('path');
 var webpack = require('webpack');
 var webpackIsomorphicTools = require('webpack-isomorphic-tools/plugin');
-
 var webpack_isomorphic_tools_plugin = new webpackIsomorphicTools(require('./webpack-isomorphic-config'));
 
 module.exports = {
@@ -25,7 +24,8 @@ module.exports = {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
       { test: /\.json$/, loader: 'json-loader'},
-      { test: webpack_isomorphic_tools_plugin.regular_expression('styles'), loader: 'style!css' },
+      // { test: webpack_isomorphic_tools_plugin.regular_expression('styles'), loader: 'style!css' },
+      { test: webpack_isomorphic_tools_plugin.regular_expression('style_modules'), loader: 'style!css' },
       { test: webpack_isomorphic_tools_plugin.regular_expression('images'), loader: 'url-loader?limit=10240'},
       { test: webpack_isomorphic_tools_plugin.regular_expression('svg'), loader: 'url-loader?limit=10240'},
       { test: webpack_isomorphic_tools_plugin.regular_expression('fonts'), loader: 'url-loader?limit=10240' }
